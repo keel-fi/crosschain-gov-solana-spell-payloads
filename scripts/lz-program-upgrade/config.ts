01ad334d49a368dc9de135a4d3eda75c04b48be5
Matt Auer <mattauer@umich.edu>
2025-12-10 10:12:27 -0600

    try surfpool

--- scripts/lz-program-upgrade/config.ts
+++ scripts/lz-program-upgrade/config.ts
@@ -2,9 +2,6 @@ import {
   KEEL_DEPLOYER,
   Network,
   SKY_LZ_GOVERNANCE_PROGRAM_ID,
-  SKY_LZ_GOVERNANCE_CPI_AUTHORITY,
-  USDS_LZ_OFT_STORE,
-  SKY_LZ_GOVERNANCE_ACCOUNT,
 } from "../../src";
 
 export const ACTION = "lz-program-upgrade";
@@ -34,10 +31,8 @@ export const NETWORK_CONFIGS: Record<Network, ProgramUpgrade> = {
     programAddress: "ALM1JSnEhc5PkNecbSZotgprBuJujL5objTbwGtpTgTd",
     programDataAddress: "93qn8rcvZPXGaHmetHrPEd6E5KuzGYEDV7bBgiyC31zj",
     programUpgradeAuthority: "FDFsoboECfazAq1eLvwBLZXhoxjPvTy4wCzPdbqnk7Zk",
-    // NOTE: ntt-mainnet.so was downloaded with:
-    // `solana program dump 43Ggis1nd29QdZFNXQAhhKKj3nxEtwN1DnbNiLf1VfEy ntt-mainnet.so -u m`
     // Dummy account.  TODO: Replace with actual program buffer.
-    newProgramBuffer: "9g2VA38gRTvVvPXQPiUVcPH4HGPMVCRvax5HKVEaBLta",
+    newProgramBuffer: "5tBFpZxv7JA1fwikZftymiMuUopZo3hXPyS5NrKYQvDF",
     spillAccount: KEEL_DEPLOYER,
     payer: KEEL_DEPLOYER,
   },
